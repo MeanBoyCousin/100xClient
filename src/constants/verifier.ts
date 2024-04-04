e1ebec4946a856c98cec72418acb75188bce33df
Tim <timothy.dunphy@gmail.com>
2024-04-04 11:00:03 +0100

    master: Contract addresses.

---
+++ src/constants/verifier.ts
@@ -0,0 +1,8 @@
+import type { HexString } from 'src/types'
+
+const verifier = {
+  mainnet: '0x0',
+  testnet: '0x65CbB566D1A6E60107c0c7888761de1AdFa1ccC0',
+} as Record<string, HexString>
+
+export default verifier
