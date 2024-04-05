383371a09b17ba0aa06c46b1f0ffa2e3b3a3c3eb
Tim <timothy.dunphy@gmail.com>
2024-04-05 18:32:20 +0100

    master: Use Environment type.

--- src/constants/ciao.ts
+++ src/constants/ciao.ts
@@ -1,8 +1,8 @@
-import type { HexString } from 'src/types'
+import type { Environment, HexString } from 'src/types'
 
 const CIAO_ADDRESS = {
   mainnet: '0x0',
   testnet: '0x63bD0ca355Cfc117F5176E5eF3e34A6D60081937',
-} as Record<string, HexString>
+} as Record<Environment, HexString>
 
 export default CIAO_ADDRESS
