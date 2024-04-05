c50513110d6e1fa3a541f9696c4245aa2bf329ad
Tim <timothy.dunphy@gmail.com>
2024-04-05 18:32:20 +0100

    master: Use Environment type.

--- src/constants/verifier.ts
+++ src/constants/verifier.ts
@@ -1,8 +1,8 @@
-import type { HexString } from 'src/types'
+import type { Environment, HexString } from 'src/types'
 
 const VERIFIER_ADDRESS = {
   mainnet: '0x0',
   testnet: '0x65CbB566D1A6E60107c0c7888761de1AdFa1ccC0',
-} as Record<string, HexString>
+} as Record<Environment, HexString>
 
 export default VERIFIER_ADDRESS
