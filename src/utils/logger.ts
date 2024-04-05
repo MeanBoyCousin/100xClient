4a3cfd73c5ecd16d99d151e3283716756d29b06a
Tim <timothy.dunphy@gmail.com>
2024-04-05 13:59:16 +0100

    master: Switch to logging lib and some refactors.

--- src/utils/logger.ts
+++
@@ -1,8 +0,0 @@
-const logger = {
-  failure: (msg: string) => console.error(`❗ `, msg),
-  message: (msg: string) => console.log(`\x1b[33m⟶\x1b[0m  `, msg),
-  success: (msg: string) => console.log(`✨ `, msg),
-  waiting: (msg: string) => console.log(`⌛ `, msg),
-}
-
-export default logger
--- stryker/stryker-logger-ignorer.js
+++ stryker/stryker-logger-ignorer.js
@@ -3,23 +3,31 @@ import { PluginKind, declareValuePlugin } from '@stryker-mutator/api/plugin'
 export const strykerPlugins = [
   declareValuePlugin(PluginKind.Ignore, 'logger', {
     shouldIgnore(path) {
-      if (
-        path.isExpressionStatement() &&
-        path.node.expression.type === 'AwaitExpression' &&
-        path.node.expression.argument.callee.property?.id &&
-        path.node.expression.argument.callee.property.id.name === 'waitForTransaction'
-      ) {
-        return "We're not interested in testing the implementation of the logger.waiting utility in the #waitForTransaction method."
-      }
+      if (path.isExpressionStatement()) {
+        if (
+          path.node.expression.type === 'AssignmentExpression' &&
+          path.node.expression.right.callee &&
+          path.node.expression.right.callee.name === 'logger'
+        ) {
+          return "We're not interested in testing the implementation of the pino logger library."
+        }
 
-      if (
-        path.isExpressionStatement() &&
-        path.node.expression.type === 'CallExpression' &&
-        path.node.expression.callee.type === 'MemberExpression' &&
-        path.node.expression.callee.object.type === 'Identifier' &&
-        path.node.expression.callee.object.name === 'logger'
-      ) {
-        return "We're not interested in testing the implementation of the logger utility."
+        if (
+          path.node.expression.type === 'AwaitExpression' &&
+          path.node.expression.argument.callee.property?.id &&
+          path.node.expression.argument.callee.property.id.name === 'waitForTransaction'
+        ) {
+          return "We're not interested in testing the implementation of the logger in the #waitForTransaction method."
+        }
+
+        if (
+          path.node.expression.callee?.object &&
+          path.node.expression.callee.object.property &&
+          path.node.expression.callee.object.property.type === 'PrivateName' &&
+          path.node.expression.callee.object.property.id.name === 'logger'
+        ) {
+          return "We're not interested in testing the implementation of the pino logger library."
+        }
       }
     },
   }),
