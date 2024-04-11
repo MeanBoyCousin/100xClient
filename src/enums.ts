964f62772293abb9713b5bc70dae691e042dc11b
Tim <timothy.dunphy@gmail.com>
2024-04-11 10:18:37 +0100

    orders-endpoints: New enums and include in build.

---
+++ src/enums.ts
@@ -0,0 +1,41 @@
+enum Environment {
+  MAINNET = 'mainnet',
+  TESTNET = 'testnet',
+}
+
+enum Interval {
+  '1M' = '1m',
+  '5M' = '5m',
+  '15M' = '15m',
+  '30M' = '30m',
+  '1H' = '1h',
+  '2H' = '2h',
+  '4H' = '4h',
+  '8H' = '8h',
+  '1D' = '1d',
+  '3D' = '3d',
+  '1W' = '1w',
+}
+
+enum OrderStatus {
+  CANCELED = 'CANCELLED',
+  EXPIRED = 'EXPIRED',
+  FILLED = 'FILLED',
+  OPEN = 'OPEN',
+  PENDING = 'PENDING',
+  REJECTED = 'REJECTED',
+}
+
+enum OrderType {
+  LIMIT = 0,
+  LIMIT_MAKER = 1,
+  MARKET = 2,
+}
+
+enum TimeInForce {
+  GTC = 0,
+  FOK = 1,
+  IOC = 2,
+}
+
+export { Environment, Interval, OrderStatus, OrderType, TimeInForce }
