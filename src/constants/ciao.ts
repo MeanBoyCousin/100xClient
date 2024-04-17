853fdb5214637e8942c7f1b6fac03561c6d04d09
Tim <timothy.dunphy@gmail.com>
2024-04-17 21:21:12 +0100

    orders-endpoints: Contract updates.

--- src/constants/ciao.ts
+++ src/constants/ciao.ts
@@ -2,7 +2,7 @@ import type { Environment, HexString } from 'src/types'
 
 const CIAO_ADDRESS = {
   mainnet: '0x63bD0ca355Cfc117F5176E5eF3e34A6D60081937',
-  testnet: '0x44034C668FfcE5b4dA5AA4B24cD936244dEa3aC8',
+  testnet: '0x0c3b9472b3923CfE199bAE24B5f5bD75FAD2bae9',
 } as Record<Environment, HexString>
 
 export default CIAO_ADDRESS
--- src/constants/verifier.ts
+++ src/constants/verifier.ts
@@ -2,7 +2,7 @@ import type { Environment, HexString } from 'src/types'
 
 const VERIFIER_ADDRESS = {
   mainnet: '0x65CbB566D1A6E60107c0c7888761de1AdFa1ccC0',
-  testnet: '0xdFd98d222377B2970Fc3e7748FA6bB31518AA49e',
+  testnet: '0x02Ca4fcB63E2D3C89fa20D86ccDcfc540c683545',
 } as Record<Environment, HexString>
 
 export default VERIFIER_ADDRESS
