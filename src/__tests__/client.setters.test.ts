db640ab913438f489e989beef41aaa293b35099a
Tim <timothy.dunphy@gmail.com>
2024-04-24 12:49:08 +0100

    v1.0.3: Allow sub-account hot swapping.

---
+++ src/__tests__/client.setters.test.ts
@@ -0,0 +1,38 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import HundredXClient from 'src'
+import { privateKey } from 'vitest/utils'
+
+describe('The HundredXClient', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  it('should allow the sub-account to be changed', async () => {
+    const Client = new HundredXClient(privateKey)
+
+    await Client.placeOrder({
+      isBuy: true,
+      price: 3450,
+      productId: 1002,
+      quantity: 0.001,
+    })
+    const firstSubAccountId = JSON.parse(fetchMock.mock.calls[0][1]?.body as string).subAccountId
+
+    expect(Client.subAccountId).toEqual(1)
+    expect(firstSubAccountId).toEqual(1)
+
+    Client.setSubAccountId = 2
+
+    await Client.placeOrder({
+      isBuy: true,
+      price: 3450,
+      productId: 1002,
+      quantity: 0.001,
+    })
+    const secondSubAccountId = JSON.parse(fetchMock.mock.calls[1][1]?.body as string).subAccountId
+
+    expect(Client.subAccountId).toEqual(2)
+    expect(secondSubAccountId).toEqual(2)
+  })
+})
