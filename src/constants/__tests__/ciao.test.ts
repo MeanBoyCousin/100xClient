b3f5f47f1409c011833a858ccb1bd1287e221cdd
Tim <timothy.dunphy@gmail.com>
2024-05-28 13:09:17 +0100

    v1.1.0: No longer mocking addresses and API URL.

--- src/constants/__tests__/ciao.test.ts
+++ src/constants/__tests__/ciao.test.ts
@@ -6,8 +6,8 @@ describe('The CIAO_ADDRESS constant', () => {
   it('should stay unchanged', () => {
     expect(CIAO_ADDRESS).toMatchInlineSnapshot(`
       {
-        "mainnet": "0x63bD0ca355Cfc117F5176E5eF3e34A6D60081937",
-        "testnet": "0x63bD0ca355Cfc117F5176E5eF3e34A6D60081937",
+        "mainnet": "0x1baebee6b00b3f559b0ff0719b47e0af22a6bfc4",
+        "testnet": "0x0c3b9472b3923cfe199bae24b5f5bd75fad2bae9",
       }
     `)
   })
--- vitest/utils.ts
+++ vitest/utils.ts
@@ -5,7 +5,7 @@ export const address = '0xb47B0b1e44B932Ae9Bb01817E7010A553A965Ea8'
 export const privateKey = '0xa608cd43cbc3d59bc51443f475b96a4654e956d6cc91783598a8e76a34000174'
 
 // Contract addresses
-export const ciaoAddress = '0x63bD0ca355Cfc117F5176E5eF3e34A6D60081937'
+export const ciaoAddress = '0x0c3b9472b3923cfe199bae24b5f5bd75fad2bae9'
 export const USDB = '0x79a59c326c715ac2d31c169c85d1232319e341ce'
 
 // API response mocks
