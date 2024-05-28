2368432976a66d998ed4a2f592b2a48863c8ba42
Tim <timothy.dunphy@gmail.com>
2024-05-28 12:41:36 +0100

    v1.1.0: Add mainnet contract addresses.

--- src/constants/marginAssets.ts
+++ src/constants/marginAssets.ts
@@ -5,7 +5,7 @@ import { MarginAssets } from 'src/enums'
 type MarginAssetKey = keyof typeof MarginAssets
 
 const MARGIN_ASSETS: Record<Environment, Record<MarginAssetKey, HexString>> = {
-  mainnet: { [MarginAssets.USDB]: '0x79a59c326c715ac2d31c169c85d1232319e341ce' },
+  mainnet: { [MarginAssets.USDB]: '0x4300000000000000000000000000000000000003' },
   testnet: { [MarginAssets.USDB]: '0x79a59c326c715ac2d31c169c85d1232319e341ce' },
 }
 
